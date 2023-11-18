9fe9961b9d35e98a89bed298c30008fa4e6b7cdb
Abdul Azis <abdul.azizz1108@gmail.com>
2023-11-18 17:31:37 +0700

    GetThreadByIdUseCase created

--- src/Applications/use_case/threads/GetThreadByIdUseCase.js
+++ src/Applications/use_case/threads/GetThreadByIdUseCase.js
@@ -1,3 +1,11 @@
-class GetThreadByIdUseCase {}
+class GetThreadByIdUseCase {
+  constructor ({ threadRepository }) {
+    this._threadRepository = threadRepository
+  }
+
+  async execute (owner, threadId) {
+    return await this._threadRepository.getThreadById(owner, threadId)
+  }
+}
 
 module.exports = GetThreadByIdUseCase
--- src/Applications/use_case/threads/_test/GetThreadByIdUseCase.test.js
+++ src/Applications/use_case/threads/_test/GetThreadByIdUseCase.test.js
@@ -1,8 +1,57 @@
-// describe('GetThreadByIdUseCase', () => {
-//   it('should orchestrating the get thread by id action correctly', async () => {
-//     // Arrange
-//     const useCasePayload = {
-//       threadId: 'thread-123'
-//     }
-//   })
-// })
+const ThreadRepository = require('../../../../Domains/threads/ThreadRepository')
+const Comment = require('../../../../Domains/threads/entities/Comment')
+const ThreadDetail = require('../../../../Domains/threads/entities/ThreadDetail')
+const GetThreadByIdUseCase = require('../GetThreadByIdUseCase')
+
+describe('GetThreadByIdUseCase', () => {
+  it('should orchestrating the get thread by id action correctly', async () => {
+    // Arrange
+    const owner = 'user-123'
+    const threadId = 'thread-123'
+    const mockThreadDetail = new ThreadDetail({
+      id: 'thread-h_2FkLZhtgBKY2kh4CC02',
+      title: 'sebuah thread',
+      body: 'sebuah body thread',
+      date: '2021-08-08T07:19:09.775Z',
+      username: 'dicoding',
+      comments: [
+        new Comment({
+          id: 'comment-_pby2_tmXV6bcvcdev8xk',
+          username: 'johndoe',
+          date: '2021-08-08T07:22:33.555Z',
+          content: 'sebuah comment'
+        })
+      ]
+    })
+
+    const mockThreadRepository = new ThreadRepository()
+
+    mockThreadRepository.getThreadById = jest.fn()
+      .mockImplementation(() => Promise.resolve(mockThreadDetail))
+
+    const getThreadUseCase = new GetThreadByIdUseCase({
+      threadRepository: mockThreadRepository
+    })
+
+    // Action
+    const threadDetail = await getThreadUseCase.execute(owner, threadId)
+
+    // Assert
+    expect(threadDetail).toStrictEqual(new ThreadDetail({
+      id: 'thread-h_2FkLZhtgBKY2kh4CC02',
+      title: 'sebuah thread',
+      body: 'sebuah body thread',
+      date: '2021-08-08T07:19:09.775Z',
+      username: 'dicoding',
+      comments: [
+        new Comment({
+          id: 'comment-_pby2_tmXV6bcvcdev8xk',
+          username: 'johndoe',
+          date: '2021-08-08T07:22:33.555Z',
+          content: 'sebuah comment'
+        })
+      ]
+    }))
+    expect(mockThreadRepository.getThreadById).toBeCalledWith(owner, threadId)
+  })
+})
