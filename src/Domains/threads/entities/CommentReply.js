b0c69a1da39dd3d480cee9e123194a8f2ea6b7bd
Abdul Azis <abdul.azizz1108@gmail.com>
2023-11-26 15:51:18 +0700

    CommentReply entity created

---
+++ src/Domains/threads/entities/CommentReply.js
@@ -0,0 +1,25 @@
+class CommentReply {
+  constructor (payload) {
+    this._validatePayload(payload)
+
+    this.id = payload.id
+    this.username = payload.username
+    this.date = payload.date
+    this.content = payload.content
+  }
+
+  _validatePayload (payload) {
+    const { id, username, date, content } = payload
+
+    if (!id || !username || !date || !content) {
+      throw new Error('COMMENT_REPLY.NOT_CONTAIN_NEEDED_PROPERTY')
+    }
+
+    if (typeof id !== 'string' || typeof username !== 'string' ||
+          typeof date !== 'string' || typeof content !== 'string') {
+      throw new Error('COMMENT_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION')
+    }
+  }
+}
+
+module.exports = CommentReply
---
+++ src/Domains/threads/entities/_test/CommentReply.test.js
@@ -0,0 +1,47 @@
+const CommentReply = require('../CommentReply')
+
+describe('a CommentReply entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      id: 'id',
+      username: 'username',
+      date: 'date'
+    }
+
+    // Action and Assert
+    expect(() => new CommentReply(payload)).toThrowError('COMMENT_REPLY.NOT_CONTAIN_NEEDED_PROPERTY')
+  })
+
+  it('should throw error when payload did not meet type spesification', () => {
+    // Arrange
+    const payload = {
+      id: true,
+      username: 123,
+      date: 123,
+      content: 123
+    }
+
+    // Action and Assert
+    expect(() => new CommentReply(payload)).toThrowError('COMMENT_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION')
+  })
+
+  it('should create comment reply object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'id-123',
+      username: 'username',
+      date: 'date',
+      content: 'content'
+    }
+
+    // Action
+    const { id, username, date, content } = new CommentReply(payload)
+
+    // Assert
+    expect(id).toEqual(payload.id)
+    expect(username).toEqual(payload.username)
+    expect(date).toEqual(payload.date)
+    expect(content).toEqual(payload.content)
+  })
+})
