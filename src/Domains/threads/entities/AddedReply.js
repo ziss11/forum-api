6149910948f473ca147c94345f91d9ce180840b3
Abdul Azis <abdul.azizz1108@gmail.com>
2023-11-26 09:52:58 +0700

    AddedReply entity created

---
+++ src/Domains/threads/entities/AddedReply.js
@@ -0,0 +1,23 @@
+class AddedReply {
+  constructor (payload) {
+    this._validatePayload(payload)
+
+    this.id = payload.id
+    this.content = payload.content
+    this.owner = payload.owner
+  }
+
+  _validatePayload (payload) {
+    const { id, content, owner } = payload
+
+    if (!id || !content || !owner) {
+      throw new Error('ADDED_REPLY.NOT_CONTAIN_NEEDED_PROPERTY')
+    }
+
+    if (typeof id !== 'string' || typeof content !== 'string' || typeof owner !== 'string') {
+      throw new Error('ADDED_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION')
+    }
+  }
+}
+
+module.exports = AddedReply
