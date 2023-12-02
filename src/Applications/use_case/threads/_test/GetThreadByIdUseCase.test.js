236ed1496f4f551a08bf4f34b079f81723852ef9
Abdul Azis <abdul.azizz1108@gmail.com>
2023-12-02 13:45:17 +0700

    re-covered uncovered testing

--- src/Applications/use_case/threads/_test/GetThreadByIdUseCase.test.js
+++ src/Applications/use_case/threads/_test/GetThreadByIdUseCase.test.js
@@ -7,46 +7,7 @@ const GetThreadByIdUseCase = require('../GetThreadByIdUseCase')
 const CommentReply = require('../../../../Domains/replies/entities/CommentReply')
 
 describe('GetThreadByIdUseCase', () => {
-  const generateComment = (comment, replies) => {
-    const commentsWithReplies = comment.map((commentItem) => {
-      const commentReplies = replies
-        .filter((reply) => reply.commentId === commentItem.id)
-        .map((reply) => {
-          return {
-            id: reply.id,
-            username: reply.username,
-            date: reply.date,
-            content: reply.isDelete ? '**balasan telah dihapus**' : reply.content
-          }
-        })
-      return {
-        id: commentItem.id,
-        username: commentItem.username,
-        date: commentItem.date,
-        content: commentItem.isDelete ? '**komentar telah dihapus**' : commentItem.content,
-        replies: commentReplies
-      }
-    })
-    return commentsWithReplies
-  }
-
-  it('should generate comments with replies property', () => {
-    // Arrange
-    const comments = [
-      { id: 1, username: 'user1', date: '2023-12-01', content: 'Comment 1', isDelete: false }
-    ]
-    const replies = [
-      { id: 1, commentId: 1, username: 'user2', date: '2023-12-02', content: 'Reply to comment 1', isDelete: false }
-    ]
-
-    // Action
-    const result = generateComment(comments, replies)
-
-    // Assert
-    expect(result).toHaveLength(1)
-  })
-
-  it('should orchestrating the get thread by id action correctly', async () => {
+  it('should disguise comment and reply content when isDelete is true', async () => {
     // Arrange
     const threadId = 'thread-123'
     const mockReplies = [
@@ -116,6 +77,85 @@ describe('GetThreadByIdUseCase', () => {
                 username: 'johndoe',
                 date: '2021-08-08T07:22:33.555Z',
                 content: '**balasan telah dihapus**'
+              })
+            ]
+          })
+        ]
+      })
+    )
+    expect(mockThreadRepository.getThreadById).toBeCalledWith(threadId)
+    expect(mockCommentRepository.getCommentByThreadId).toBeCalledWith(threadId)
+    expect(mockReplyRepository.getCommentsReplyByThreadId).toBeCalledWith(threadId)
+  })
+
+  it('should orchestrating the get thread by id action correctly', async () => {
+    // Arrange
+    const threadId = 'thread-123'
+    const mockReplies = [
+      new CommentReply({
+        id: 'reply-_pby2_tmXV6bcvcdev8xk',
+        commentId: 'comment-_pby2_tmXV6bcvcdev8xk',
+        username: 'johndoe',
+        date: '2021-08-08T07:22:33.555Z',
+        content: 'sebuah balasan komentar'
+      })
+    ]
+    const mockComments = [
+      new Comment({
+        id: 'comment-_pby2_tmXV6bcvcdev8xk',
+        username: 'johndoe',
+        date: '2021-08-08T07:22:33.555Z',
+        content: 'sebuah comment'
+      })
+    ]
+    const mockThreadDetail = new ThreadDetail({
+      id: 'thread-h_2FkLZhtgBKY2kh4CC02',
+      title: 'sebuah thread',
+      body: 'sebuah body thread',
+      date: '2021-08-08T07:19:09.775Z',
+      username: 'dicoding'
+    })
+
+    const mockThreadRepository = new ThreadRepository()
+    const mockCommentRepository = new CommentRepository()
+    const mockReplyRepository = new ReplyRepository()
+
+    mockThreadRepository.getThreadById = jest.fn()
+      .mockImplementation(() => Promise.resolve(mockThreadDetail))
+    mockCommentRepository.getCommentByThreadId = jest.fn()
+      .mockImplementation(() => Promise.resolve(mockComments))
+    mockReplyRepository.getCommentsReplyByThreadId = jest.fn()
+      .mockImplementation(() => Promise.resolve(mockReplies))
+
+    const getThreadUseCase = new GetThreadByIdUseCase({
+      threadRepository: mockThreadRepository,
+      commentRepository: mockCommentRepository,
+      replyRepository: mockReplyRepository
+    })
+
+    // Action
+    const threadDetail = await getThreadUseCase.execute(threadId)
+
+    // Assert
+    expect(threadDetail).toStrictEqual(
+      new ThreadDetail({
+        id: 'thread-h_2FkLZhtgBKY2kh4CC02',
+        title: 'sebuah thread',
+        body: 'sebuah body thread',
+        date: '2021-08-08T07:19:09.775Z',
+        username: 'dicoding',
+        comments: [
+          new Comment({
+            id: 'comment-_pby2_tmXV6bcvcdev8xk',
+            username: 'johndoe',
+            date: '2021-08-08T07:22:33.555Z',
+            content: 'sebuah comment',
+            replies: [
+              new CommentReply({
+                id: 'reply-_pby2_tmXV6bcvcdev8xk',
+                username: 'johndoe',
+                date: '2021-08-08T07:22:33.555Z',
+                content: 'sebuah balasan komentar'
               })
             ]
           })
--- src/Commons/exceptions/DomainErrorTranslator.js
+++ src/Commons/exceptions/DomainErrorTranslator.js
@@ -7,6 +7,8 @@ const DomainErrorTranslator = {
 }
 
 DomainErrorTranslator._directories = {
+  'NEW_AUTH.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('harus mengirimkan accessToken dan refreshToken'),
+  'NEW_AUTH.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('accessToken dan refreshToken harus string'),
   'REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'),
   'REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai'),
   'REGISTER_USER.USERNAME_LIMIT_CHAR': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit'),
