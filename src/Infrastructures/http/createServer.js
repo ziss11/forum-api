474cd2a8d2115474c9d371dad9401bdbd69dcc7f
Abdul Azis <abdul.azizz1108@gmail.com>
2023-12-20 12:13:22 +0700

    pass say hello world test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -51,13 +51,13 @@ const createServer = async (container) => {
         },
     ]);
 
-    // server.route({
-    //     method: 'GET',
-    //     path: '/',
-    //     handler: () => ({
-    //         value: 'Hello world!',
-    //     }),
-    // });
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: () => ({
+            value: 'Hello world!',
+        }),
+    });
 
     server.ext('onPreResponse', (request, h) => {
         // mendapatkan konteks response dari request
